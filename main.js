d2e237879c9e9298e9961a297c6ba71d9ce437e2
rollahaideri <rohullahhaideri@gmail.com>
2021-09-29 23:58:43 +0200

    Have some new test with js

---
+++ main.js
@@ -0,0 +1,63 @@
+// check if the number is positive or negative/zero
+
+const number = prompt("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+  console.log("The number is positive");
+}
+// if number is not greater than 0
+else {
+  console.log("The number is either a negative number or 0");
+}
+
+console.log("The if...else statement is easy");
+
+
+function changeBackgroundColor (element, color) {
+    element.style.backgroundColor = color;
+}
+
+const myBox = document.getElementById("myBox");
+
+changeBackgroundColor(myBox, "red");
+
+prompt("Tryck här för att fortsätta")
+
+
+// program to display numbers from 1 to 5
+const n = 5;
+
+// looping from i = 1 to 5
+// in each iteration, i is increased by 1
+for (let i = 1; i <= n; i++) {
+    console.log(i);     // printing the value of i
+}
+
+const student = {
+    name: 'Monica',
+    class: 7,
+    age: 12
+}
+
+// using for...in
+for ( let key in student ) {
+
+    // display the properties
+    console.log(`${key} => ${student[key]}`);
+}
+
+// program to show try...catch in a program
+
+const numerator= 100, denominator = 'a';
+
+try {
+     console.log(numerator/denominator);
+
+    // forgot to define variable a      
+    console.log(a);
+}
+catch(error) {
+    console.log('An error caught'); 
+    console.log('Error message: ' + error);  
+}
